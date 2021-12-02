dc01d6377d8dd81ff33e46feea3c7aa039c8c40d
Jordan Porter <53372392+JordanPorterDev@users.noreply.github.com>
2021-12-02 21:24:16 +0000

    adding getAllProducts api pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,12 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10)
+    .skip(5)
+
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -16,13 +21,13 @@ const getAllProducts = async (req,res) => {
     const queryObject = {}
 
     if(featured) {
-        queryObject.featured = featured === 'true'? true : false
+        queryObject.featured = featured === 'true' ? true : false
     }
     if(company) {
         queryObject.company = company
     }
     if(name) {
-        queryObject.name = {$regex: name, $options: 'i'}
+        queryObject.name = { $regex: name, $options: 'i' }
     }
     // console.log(queryObject)
     let result = Product.find(queryObject)
@@ -38,8 +43,14 @@ const getAllProducts = async (req,res) => {
     if(fields){
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList)
-
     }   
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
+
     
     const products = await result
     res.status(200).json({products, nbHits: products.length})
